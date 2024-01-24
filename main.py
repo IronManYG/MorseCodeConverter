d8195cfcc154c03cd0de290de81628cac0147c62
IronManYG <hussain.yg2010@gmail.com>
2024-01-24 14:44:38 +0300

    Create converter and it works.

---
+++ main.py
@@ -0,0 +1,16 @@
+from converter import MorseCodeConverter
+from morse_code_data import international_code  # Import the dictionary
+
+international_converter = MorseCodeConverter(international_code)
+
+print('Welcome to the International Morse Code Converter')
+
+convert_message = True
+while convert_message:
+    message = input("Write your message: ")
+    morse_message = international_converter.to_morse_code(message)
+    print(f"Your Morse code: {morse_message}")
+
+    continue_input = input("Do you want to convert another message? (Yes/No): ").lower()
+    if continue_input not in ['y', 'yes']:
+        convert_message = False
